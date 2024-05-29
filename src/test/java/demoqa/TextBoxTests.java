2ee86a721c338524039ebdda5e08e75254e0f177
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-05-29 14:56:35 +0300

    Добавил тестовые методы fillFormTest и before

--- src/test/java/demoqa/TextBoxTests.java
+++ src/test/java/demoqa/TextBoxTests.java
@@ -1,16 +1,49 @@
 package demoqa;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTests {
+    String name = "Alex Haikin";
+    String email = "al.hai@mail.ru";
+    String currentAddress = "Russia, Moscow";
+    String permanentAddress = "France, Nàntt";
+
+    @BeforeAll
+    static void before() {
+        //Configuration.browser = "firefox";
+        //Configuration.pageLoadStrategy = "none";
+        //Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadTimeout = 60000;
+        //Configuration.holdBrowserOpen = true;
+        //Configuration.headless = true;
+        Configuration.baseUrl = "https://demoqa.com";
+    }
 
     @Test
     void fillFormTest() {
-        Configuration.browser = "firefox";
-        open("https://demoqa.com/text-box");
+        open("/text-box");
+        $(By.cssSelector(".text-center")).shouldHave(text("Text Box"));
+
+        $(By.cssSelector("#userName")).setValue(name);
+        $(By.cssSelector("#userEmail")).setValue(email);
+        $(By.cssSelector("#currentAddress")).setValue(currentAddress);
+        $(By.cssSelector("#permanentAddress")).setValue(permanentAddress);
+        $(By.cssSelector("#submit")).scrollTo();
+        $(By.cssSelector("#submit")).click();
+
+        $(By.cssSelector("#output")).shouldBe(visible);
+        $(By.cssSelector("#output")).$(By.cssSelector("#name")).shouldHave(text(name));
+        $(By.cssSelector("#output")).$(By.cssSelector("#email")).shouldHave(text(email));
+        $(By.cssSelector("#output")).$(By.cssSelector("#currentAddress")).shouldHave(text(currentAddress));
+        $(By.cssSelector("#output")).$(By.cssSelector("#permanentAddress")).shouldHave(text(permanentAddress));
+        String result = $(By.cssSelector("#output")).$(By.cssSelector("#permanentAddress")).getText();
+        System.out.println(result);
+
     }
 }
