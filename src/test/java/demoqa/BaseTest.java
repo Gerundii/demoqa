3330e9c1acbfb6117f6a95240fd165f66a0e5122
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-05-29 15:13:28 +0300

    Добавил класс BaseTest, в который вынес метод before, общий для все будущих тестов

---
+++ src/test/java/demoqa/BaseTest.java
@@ -0,0 +1,18 @@
+package demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+
+    @BeforeAll
+    static void before() {
+        //Configuration.browser = "firefox";
+        //Configuration.pageLoadStrategy = "none";
+        //Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadTimeout = 60000;
+        //Configuration.holdBrowserOpen = true;
+        //Configuration.headless = true;
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+}
