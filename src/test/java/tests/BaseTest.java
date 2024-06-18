2fff6926efa976298ffe6077ff386cd9228f879d
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-06-18 21:43:34 +0300

    Добавил в BaseTest создание объекта страницы RegistrationPageÑ

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,8 +2,10 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import pages.RegistrationPage;
 
 public class BaseTest {
+    RegistrationPage registrationPage = new RegistrationPage();
 
     @BeforeAll
     static void before() {
@@ -11,7 +13,7 @@ public class BaseTest {
         //Configuration.pageLoadStrategy = "none";
         //Configuration.browserSize = "1920x1080";
         Configuration.pageLoadTimeout = 60000;
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
         //Configuration.headless = true;
         Configuration.baseUrl = "https://demoqa.com";
     }
