c279527d6d317694082f764b5102c3c39706d700
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-06-18 21:46:55 +0300

    Добавил описание полейполя для имени, почты, телефона и методы, для работы с ними

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,4 +1,44 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class RegistrationPage {
+    private final String TITLE_TEXT = "Student Registration Form";
+    private SelenideElement firstNameInput = $("#firstName");
+    private SelenideElement lastNameInput = $("#lastName");
+    private SelenideElement emailInput = $("#userEmail");
+    private SelenideElement genderInput = $("#genterWrapper");
+    private SelenideElement mobileInput = $("#userNumber");
+
+    public void openPage() {
+        open("/automation-practice-form");
+
+        $(".text-center").shouldHave(text("Practice Form"));
+        $(".practice-form-wrapper h5").shouldHave(text(TITLE_TEXT));
+    }
+
+    public void setFirstName(String firstName) {
+        firstNameInput.setValue(firstName);
+    }
+
+    public void setLastName(String lastName) {
+        lastNameInput.setValue(lastName);
+    }
+
+    public void setEmail(String email) {
+        emailInput.setValue(email);
+    }
+
+    public void setGender(String gender) {
+        genderInput.$(byText(gender)).click();
+    }
+
+    public void setMobile(String phoneNumber) {
+        mobileInput.setValue(phoneNumber);
+    }
 }
