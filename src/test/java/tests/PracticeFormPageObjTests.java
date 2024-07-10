d5e62ed2461f7b646b153652e0e37341220c5c0f
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-07-10 19:49:37 +0300

    Добавил тест с использованием fluent интерфейса

--- src/test/java/tests/PracticeFormPageObjTests.java
+++ src/test/java/tests/PracticeFormPageObjTests.java
@@ -74,4 +74,36 @@ public class PracticeFormPageObjTests extends BaseTest {
         $(".table-responsive").$(byText("Address")).sibling(0).shouldHave(text(currentAddress));
         $(".table-responsive").$(byText("State and City")).sibling(0).shouldHave(text(state + " " + city));
     }
+
+    @Test
+    void fillPracticeFormTestWithFluent() {
+
+        registrationPageWithFluent.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGender(gender)
+                .setMobile(phoneNumber)
+                .setBirthDate(birthDate, birthMonth, birthYear)
+                .setSubject(subject)
+                .setHobby(hobby)
+                .setPicture(picture)
+                .setCurrentAddress(currentAddress)
+                .setState(state)
+                .setCity(city)
+                .submit()
+                .verifyRegistrationResultAppear();
+
+        //Assertions
+        $(".table-responsive").$(byText("Student Name")).sibling(0).shouldHave(text(firstName + " " + lastName));
+        $(".table-responsive").$(byText("Student Email")).sibling(0).shouldHave(text(email));
+        $(".table-responsive").$(byText("Gender")).sibling(0).shouldHave(text(gender));
+        $(".table-responsive").$(byText("Mobile")).sibling(0).shouldHave(text(phoneNumber));
+        $(".table-responsive").$(byText("Date of Birth")).sibling(0).shouldHave(text(birthDate + " " + birthMonth + "," + birthYear));
+        $(".table-responsive").$(byText("Subjects")).sibling(0).shouldHave(text(subject));
+        $(".table-responsive").$(byText("Hobbies")).sibling(0).shouldHave(text(hobby));
+        $(".table-responsive").$(byText("Picture")).sibling(0).shouldHave(text(picture));
+        $(".table-responsive").$(byText("Address")).sibling(0).shouldHave(text(currentAddress));
+        $(".table-responsive").$(byText("State and City")).sibling(0).shouldHave(text(state + " " + city));
+    }
 }
