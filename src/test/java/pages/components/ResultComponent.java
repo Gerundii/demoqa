611718b6e73a1d3f31015dbd66bde56edd2511ba
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-07-11 15:52:45 +0300

    Добавил метод проверки содержание полей формы результата регистрации

--- src/test/java/pages/components/ResultComponent.java
+++ src/test/java/pages/components/ResultComponent.java
@@ -1,11 +1,16 @@
 package pages.components;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ResultComponent {
     public void verifyResultComponentAppears(String message) {
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text(message));
+    }
+
+    public void verifyResult (String key, String value) {
+        $(".table-responsive").$(byText(key)).sibling(0).shouldHave(text(value));
     }
 }
