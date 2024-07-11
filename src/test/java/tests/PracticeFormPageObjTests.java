4596c1c19e57745b7c76c6619bd5179193fecfff
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-07-11 15:59:36 +0300

    Использовал методы проверки формы подтверждения регистрационных данных

--- src/test/java/tests/PracticeFormPageObjTests.java
+++ src/test/java/tests/PracticeFormPageObjTests.java
@@ -43,23 +43,23 @@ public class PracticeFormPageObjTests extends BaseTest {
         registrationPage.verifyRegistrationResultAppear();
 
         //Assertions
-        $(".table-responsive").$(byText("Student Name")).sibling(0).shouldHave(text(firstName + " " + lastName));
-        $(".table-responsive").$(byText("Student Email")).sibling(0).shouldHave(text(email));
-        $(".table-responsive").$(byText("Gender")).sibling(0).shouldHave(text(gender));
-        $(".table-responsive").$(byText("Mobile")).sibling(0).shouldHave(text(phoneNumber));
-        $(".table-responsive").$(byText("Date of Birth")).sibling(0).shouldHave(text(birthDate + " " + birthMonth + "," + birthYear));
-        $(".table-responsive").$(byText("Subjects")).sibling(0).shouldHave(text(subject));
-        $(".table-responsive").$(byText("Hobbies")).sibling(0).shouldHave(text(hobby));
-        $(".table-responsive").$(byText("Picture")).sibling(0).shouldHave(text(picture));
-        $(".table-responsive").$(byText("Address")).sibling(0).shouldHave(text(currentAddress));
-        $(".table-responsive").$(byText("State and City")).sibling(0).shouldHave(text(state + " " + city));
+        registrationPage.verifyRegistrationResult("Student Name", firstName + " " + lastName);
+        registrationPage.verifyRegistrationResult("Gender", gender);
+        registrationPage.verifyRegistrationResult("Mobile", phoneNumber);
+        registrationPage.verifyRegistrationResult("Date of Birth", birthDate + " " + birthMonth + "," + birthYear);
+        registrationPage.verifyRegistrationResult("Subjects", subject);
+        registrationPage.verifyRegistrationResult("Hobbies", hobby);
+        registrationPage.verifyRegistrationResult("Picture", picture);
+        registrationPage.verifyRegistrationResult("Address", currentAddress);
+        registrationPage.verifyRegistrationResult("State and City", state + " " + city);
     }
 
     @Test
     void fillPracticeFormTestWithFluent() {
 
-        registrationPageWithFluent.openPage()
-                .setFirstName(firstName)
+        registrationPageWithFluent.openPage();
+
+        registrationPageWithFluent.setFirstName(firstName)
                 .setLastName(lastName)
                 .setEmail(email)
                 .setGender(gender)
@@ -76,15 +76,14 @@ public class PracticeFormPageObjTests extends BaseTest {
         registrationPageWithFluent.verifyRegistrationResultAppear();
 
         //Assertions
-        $(".table-responsive").$(byText("Student Name")).sibling(0).shouldHave(text(firstName + " " + lastName));
-        $(".table-responsive").$(byText("Student Email")).sibling(0).shouldHave(text(email));
-        $(".table-responsive").$(byText("Gender")).sibling(0).shouldHave(text(gender));
-        $(".table-responsive").$(byText("Mobile")).sibling(0).shouldHave(text(phoneNumber));
-        $(".table-responsive").$(byText("Date of Birth")).sibling(0).shouldHave(text(birthDate + " " + birthMonth + "," + birthYear));
-        $(".table-responsive").$(byText("Subjects")).sibling(0).shouldHave(text(subject));
-        $(".table-responsive").$(byText("Hobbies")).sibling(0).shouldHave(text(hobby));
-        $(".table-responsive").$(byText("Picture")).sibling(0).shouldHave(text(picture));
-        $(".table-responsive").$(byText("Address")).sibling(0).shouldHave(text(currentAddress));
-        $(".table-responsive").$(byText("State and City")).sibling(0).shouldHave(text(state + " " + city));
+        registrationPageWithFluent.verifyRegistrationResult("Student Name", firstName + " " + lastName)
+                .verifyRegistrationResult("Gender", gender)
+                .verifyRegistrationResult("Mobile", phoneNumber)
+                .verifyRegistrationResult("Date of Birth", birthDate + " " + birthMonth + "," + birthYear)
+                .verifyRegistrationResult("Subjects", subject)
+                .verifyRegistrationResult("Hobbies", hobby)
+                .verifyRegistrationResult("Picture", picture)
+                .verifyRegistrationResult("Address", currentAddress)
+                .verifyRegistrationResult("State and City", state + " " + city);
     }
 }
