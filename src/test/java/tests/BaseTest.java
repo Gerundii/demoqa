80a3a68df71086b29b4efa041ce247728889f6be
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-07-15 20:39:18 +0300

    Добавил создание объекта класса PracticeFormDatafaker

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,15 +4,17 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import pages.RegistrationPage;
 import pages.RegistrationPageWithFluent;
+import utils.PracticeFormDatafaker;
 
 public class BaseTest {
     RegistrationPage registrationPage = new RegistrationPage();
     RegistrationPageWithFluent registrationPageWithFluent = new RegistrationPageWithFluent();
+    PracticeFormDatafaker pfData = new PracticeFormDatafaker();
 
     @BeforeAll
     static void before() {
         //Configuration.browser = "firefox";
-        //Configuration.pageLoadStrategy = "none";
+        Configuration.pageLoadStrategy = "none";
         //Configuration.browserSize = "1920x1080";
         Configuration.pageLoadTimeout = 60000;
         //Configuration.holdBrowserOpen = true;
