4244c4aac6dc73bd1d59933c4d38f09b5c70415f
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-07-16 13:40:52 +0300

    Добавил в класс с тестами для формы регистрации создание объекта класса PractiseFormDatafaker, создание объекта StudentData

--- src/test/java/tests/PracticeFormPageObjTests.java
+++ src/test/java/tests/PracticeFormPageObjTests.java
@@ -1,55 +1,62 @@
 package tests;
 
+import model.StudentData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import utils.PracticeFormDatafaker;
 
 import static utils.PracticeFormDatafaker.*;
 
 public class PracticeFormPageObjTests extends BaseTest {
-    String firstName = pfData.generateFirstName();
-    String lastName = pfData.generateLastName();
-    String email = pfData.generateEmail();
-    String gender = pfData.generateGender();
-    String phoneNumber = pfData.generatePhoneNumber();
-    String birthDate = pfData.generateBirthDate();
-    String birthMonth = pfData.generateBirthMonth();
-    String birthYear = pfData.generateBirthYear();
-    String subject = pfData.generateSubject();
-    String hobby = pfData.generateHobby();
-    String picture = "avatar.jpg";
-    String currentAddress = pfData.generateCurrentAddress();
-    String state = pfData.generateState();
-    String city = pfData.generateCity(state);
+    PracticeFormDatafaker pfData = new PracticeFormDatafaker();
+
+    StudentData student = StudentData.builder()
+            .firstName(pfData.generateFirstName())
+            .lastName(pfData.generateLastName())
+            .email(pfData.generateEmail())
+            .gender(pfData.generateGender())
+            .phoneNumber(pfData.generatePhoneNumber())
+            .birthDate(pfData.generateBirthDate())
+            .birthMonth(pfData.generateBirthMonth())
+            .birthYear(pfData.generateBirthYear())
+            .subject(pfData.generateSubject())
+            .hobby(pfData.generateHobby())
+            .picture("avatar.jpg")
+            .currentAddress(pfData.generateCurrentAddress())
+            .state(pfData.generateState())
+            .city(pfData.generateCity())
+            .build();
 
     @Test
     void fillPracticeFormTest() throws InterruptedException {
 
         registrationPage.openPage();
-        registrationPage.setFirstName(firstName);
-        registrationPage.setLastName(lastName);
-        registrationPage.setEmail(email);
-        registrationPage.setGender(gender);
-        registrationPage.setMobile(phoneNumber);
-        registrationPage.setBirthDate(birthDate, birthMonth, birthYear);
-        registrationPage.setSubject(subject);
-        registrationPage.setHobby(hobby);
-        registrationPage.setPicture(picture);
-        registrationPage.setCurrentAddress(currentAddress);
-        registrationPage.setState(state);
-        registrationPage.setCity(city);
+        registrationPage.setFirstName(student.getFirstName());
+        registrationPage.setLastName(student.getLastName());
+        registrationPage.setEmail(student.getEmail());
+        registrationPage.setGender(student.getGender());
+        registrationPage.setMobile(student.getPhoneNumber());
+        registrationPage.setBirthDate(student.getBirthDate(), student.getBirthMonth(), student.getBirthYear());
+        registrationPage.setSubject(student.getSubject());
+        registrationPage.setHobby(student.getHobby());
+        registrationPage.setPicture(student.getPicture());
+        registrationPage.setCurrentAddress(student.getCurrentAddress());
+        registrationPage.setState(student.getState());
+        registrationPage.setCity(student.getCity());
         registrationPage.submit();
 
         registrationPage.verifyRegistrationResultAppear();
 
         //Assertions
-        registrationPage.verifyRegistrationResult("Student Name", firstName + " " + lastName);
-        registrationPage.verifyRegistrationResult("Gender", gender);
-        registrationPage.verifyRegistrationResult("Mobile", phoneNumber);
-        registrationPage.verifyRegistrationResult("Date of Birth", birthDate + " " + birthMonth + "," + birthYear);
-        registrationPage.verifyRegistrationResult("Subjects", subject);
-        registrationPage.verifyRegistrationResult("Hobbies", hobby);
-        registrationPage.verifyRegistrationResult("Picture", picture);
-        registrationPage.verifyRegistrationResult("Address", currentAddress);
-        registrationPage.verifyRegistrationResult("State and City", state + " " + city);
+        registrationPage.verifyRegistrationResult("Student Name", student.getFirstName() + " " + student.getLastName());
+        registrationPage.verifyRegistrationResult("Gender", student.getGender());
+        registrationPage.verifyRegistrationResult("Mobile", student.getPhoneNumber());
+        registrationPage.verifyRegistrationResult("Date of Birth", student.getBirthDate() + " " + student.getBirthMonth() + "," + student.getBirthYear());
+        registrationPage.verifyRegistrationResult("Subjects", student.getSubject());
+        registrationPage.verifyRegistrationResult("Hobbies", student.getHobby());
+        registrationPage.verifyRegistrationResult("Picture", student.getPicture());
+        registrationPage.verifyRegistrationResult("Address", student.getCurrentAddress());
+        registrationPage.verifyRegistrationResult("State and City", student.getState() + " " + student.getCity());
     }
 
     @Test
@@ -57,31 +64,31 @@ public class PracticeFormPageObjTests extends BaseTest {
 
         registrationPageWithFluent.openPage();
 
-        registrationPageWithFluent.setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(email)
-                .setGender(gender)
-                .setMobile(phoneNumber)
-                .setBirthDate(birthDate, birthMonth, birthYear)
-                .setSubject(subject)
-                .setHobby(hobby)
-                .setPicture(picture)
-                .setCurrentAddress(currentAddress)
-                .setState(state)
-                .setCity(city)
+        registrationPageWithFluent.setFirstName(student.getFirstName())
+                .setLastName(student.getLastName())
+                .setEmail(student.getEmail())
+                .setGender(student.getGender())
+                .setMobile(student.getPhoneNumber())
+                .setBirthDate(student.getBirthDate(), student.getBirthMonth(), student.getBirthYear())
+                .setSubject(student.getSubject())
+                .setHobby(student.getHobby())
+                .setPicture(student.getPicture())
+                .setCurrentAddress(student.getCurrentAddress())
+                .setState(student.getState())
+                .setCity(student.getCity())
                 .submit();
 
         registrationPageWithFluent.verifyRegistrationResultAppear();
 
         //Assertions
-        registrationPageWithFluent.verifyRegistrationResult("Student Name", firstName + " " + lastName)
-                .verifyRegistrationResult("Gender", gender)
-                .verifyRegistrationResult("Mobile", phoneNumber)
-                .verifyRegistrationResult("Date of Birth", birthDate + " " + birthMonth + "," + birthYear)
-                .verifyRegistrationResult("Subjects", subject)
-                .verifyRegistrationResult("Hobbies", hobby)
-                .verifyRegistrationResult("Picture", picture)
-                .verifyRegistrationResult("Address", currentAddress)
-                .verifyRegistrationResult("State and City", state + " " + city);
+        registrationPageWithFluent.verifyRegistrationResult("Student Name", student.getFirstName() + " " + student.getLastName())
+                .verifyRegistrationResult("Gender", student.getGender())
+                .verifyRegistrationResult("Mobile", student.getPhoneNumber())
+                .verifyRegistrationResult("Date of Birth", student.getBirthDate() + " " + student.getBirthMonth() + "," + student.getBirthYear())
+                .verifyRegistrationResult("Subjects", student.getSubject())
+                .verifyRegistrationResult("Hobbies", student.getHobby())
+                .verifyRegistrationResult("Picture", student.getPicture())
+                .verifyRegistrationResult("Address", student.getCurrentAddress())
+                .verifyRegistrationResult("State and City", student.getState() + " " + student.getCity());
     }
 }
