5be3c546dc01db14c07c217f8e078371a676f0f4
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-07-16 12:53:37 +0300

    Изменил генерацию дня рождения студента

--- src/test/java/utils/PracticeFormDatafaker.java
+++ src/test/java/utils/PracticeFormDatafaker.java
@@ -2,89 +2,87 @@ package utils;
 
 import net.datafaker.Faker;
 
-import java.text.SimpleDateFormat;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
 public class PracticeFormDatafaker {
     Faker faker = new Faker(new Locale("en"));
 
-    String[] genders = {"Male", "Female", "Other"};
-    String[] subjects = {"Maths", "Art", "Biology", "Civics", "Physics"};
-    String[] hobbies = {"Sports", "Reading", "Music"};
-    String[] state = {"NCR", "Uttar Pradesh", "Haryana", "Rajasthan"};
-    String[] cityNCR = {"Delhi", "Gurgaon", "Noida"};
-    String[] cityUttar = {"Agra", "Lucknow", "Merrut"};
-    String[] cityHaryana = {"Karnal", "Panipat"};
-    String[] cityRajasthan = {"Jaipur", "Jaiselmer"};
+    private final String[] GENDERS = {"Male", "Female", "Other"};
+    private final String[] SUBJECTS = {"Maths", "Art", "Biology", "Civics", "Physics"};
+    private final String[] HOBBIES = {"Sports", "Reading", "Music"};
+    private final String[] STATE = {"NCR", "Uttar Pradesh", "Haryana", "Rajasthan"};
+    private final String[] CITYNCR = {"Delhi", "Gurgaon", "Noida"};
+    private final String[] CITYUttar = {"Agra", "Lucknow", "Merrut"};
+    private final String[] CITYHaryana = {"Karnal", "Panipat"};
+    private final String[] CITYRajasthan = {"Jaipur", "Jaiselmer"};
 
-    String[] birthDate = faker.timeAndDate().birthday(20, 30, "dd MMMM YYYY").split(" ");
+    String[] birthDay = generateBirthDay();
 
     public void main(String[] args) {
     }
 
-    public String generateFirstName(){
+    public String generateFirstName() {
         return faker.name().firstName();
     }
 
-    public String generateLastName(){
+    public String generateLastName() {
         return faker.name().lastName();
     }
 
-    public String generateEmail(){
+    public String generateEmail() {
         return faker.internet().emailAddress();
     }
 
-    public String generateGender(){
-        return faker.options().option(genders);
+    public String generateGender() {
+        return faker.options().option(GENDERS);
     }
 
-    public String generatePhoneNumber(){
+    public String generatePhoneNumber() {
         return String.valueOf(faker.number().numberBetween(9010000000L, 9999999999L));
     }
 
-    public String generateBirthDate(){
-        return birthDate[0];
+    public String[] generateBirthDay() {
+        return birthDay = faker.timeAndDate().birthday(20, 30, "dd MMMM YYYY").split(" ");
     }
 
-    public String generateBirthMonth(){
-        return birthDate[1];
+    public String generateBirthDate() {
+        return birthDay[0];
     }
 
-    public String generateBirthYear(){
-        return birthDate[2];
+    public String generateBirthMonth() {
+        return birthDay[1];
     }
 
-    public String generateSubject(){
-        return faker.options().option(subjects);
+    public String generateBirthYear() {
+        return birthDay[2];
     }
 
-    public String generateHobby(){
-        return faker.options().option(hobbies);
+    public String generateSubject() {
+        return faker.options().option(SUBJECTS);
     }
 
-    public String generateCurrentAddress(){
+    public String generateHobby() {
+        return faker.options().option(HOBBIES);
+    }
+
+    public String generateCurrentAddress() {
         return faker.address().fullAddress();
     }
 
-    public String generateState(){
-        return faker.options().option(state);
+    public String generateState() {
+        return faker.options().option(STATE);
     }
 
-    public String generateCity(String state){
+    public String generateCity(String state) {
         String city = null;
         if (state.equals("NCR")) {
-            city = faker.options().option(cityNCR);
-        }
-        else if (state.equals("Uttar Pradesh")) {
-            city = faker.options().option(cityUttar);
-        }
-        else if (state.equals("Haryana")) {
-            city = faker.options().option(cityHaryana);
-        }
-        else if (state.equals("Rajasthan")) {
-            city = faker.options().option(cityRajasthan);
+            city = faker.options().option(CITYNCR);
+        } else if (state.equals("Uttar Pradesh")) {
+            city = faker.options().option(CITYUttar);
+        } else if (state.equals("Haryana")) {
+            city = faker.options().option(CITYHaryana);
+        } else if (state.equals("Rajasthan")) {
+            city = faker.options().option(CITYRajasthan);
         }
         return city;
     }
